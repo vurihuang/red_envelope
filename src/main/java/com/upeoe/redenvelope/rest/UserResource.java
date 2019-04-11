09665f2b0404974faa3daecde93b54d651dbbb2e
upeoe <upeoe63@163.com>
2019-04-11 12:12:52 +0800

    Remove unused import

--- src/main/java/com/upeoe/redenvelope/rest/UserResource.java
+++ src/main/java/com/upeoe/redenvelope/rest/UserResource.java
@@ -1,12 +1,10 @@
 package com.upeoe.redenvelope.rest;
 
-import com.upeoe.redenvelope.entity.User;
 import com.upeoe.redenvelope.service.UserService;
 import com.upeoe.redenvelope.utils.ResultHolder;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
-import jdk.management.resource.internal.inst.UnixAsynchronousServerSocketChannelImplRMHooks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
